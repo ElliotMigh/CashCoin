9e3faf6fa424cd617fd0ed4a36334923db739f6e
matin <elliotmigh@gmail.com>
2023-07-31 14:30:22 +0330

    create function for show welcome toast in MainActivity.kt

--- app/src/main/java/com/example/cashcoin/MainActivity.kt
+++ app/src/main/java/com/example/cashcoin/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.cashcoin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //call welcome toast function:
+        welcomeToast()
+    }
+    private fun welcomeToast(){
+        Toast.makeText(this, "Welcome to Cash Coin:)", Toast.LENGTH_SHORT).show()
     }
 }
