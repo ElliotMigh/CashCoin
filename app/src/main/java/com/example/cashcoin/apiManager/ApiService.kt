d23198c7bb73515fadea18b49d7544cb48933133
matin <elliotmigh@gmail.com>
2023-08-10 15:16:04 +0330

    add GET anotation in ApiService.kt

--- app/src/main/java/com/example/cashcoin/apiManager/ApiService.kt
+++ app/src/main/java/com/example/cashcoin/apiManager/ApiService.kt
@@ -1,9 +1,11 @@
 package com.example.cashcoin.apiManager
 
+import retrofit2.http.GET
 import retrofit2.http.Headers
 
 interface ApiService {
 
     @Headers(API_KEY)
+    @GET("v2/news")
     fun getTopNews()
 }
