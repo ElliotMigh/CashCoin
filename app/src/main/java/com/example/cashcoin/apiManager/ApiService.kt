08feac2c9b2df026a69481197aeb2fee11509f58
matin <elliotmigh@gmail.com>
2023-08-10 15:21:05 +0330

    add query parameters sortOrder in interface getTopNews

--- app/src/main/java/com/example/cashcoin/apiManager/ApiService.kt
+++ app/src/main/java/com/example/cashcoin/apiManager/ApiService.kt
@@ -2,10 +2,13 @@ package com.example.cashcoin.apiManager
 
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Query
 
 interface ApiService {
 
     @Headers(API_KEY)
     @GET("v2/news")
-    fun getTopNews()
+    fun getTopNews(
+        @Query("sortOrder") sortOrder: String
+    )
 }
