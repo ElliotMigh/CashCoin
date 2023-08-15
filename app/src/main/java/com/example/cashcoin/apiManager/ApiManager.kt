3f140b6f2f874f64cdadc3ec1db31aa4d9a6dc73
matin <elliotmigh@gmail.com>
2023-08-15 10:13:48 +0330

    create function getNews in ApiManager.kt

--- app/src/main/java/com/example/cashcoin/apiManager/ApiManager.kt
+++ app/src/main/java/com/example/cashcoin/apiManager/ApiManager.kt
@@ -1,11 +1,16 @@
 package com.example.cashcoin.apiManager
 
+import com.example.cashcoin.apiManager.model.NewsData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 const val BASE_URL = "https://min-api.cryptocompare.com/data/"
 const val BASE_URL_IMAGE = "https://www.cryptocompare.com"
-const val API_KEY = "authorization: Apikey 79a0afc8e1acf0c14d61b201c82c5d491e7a8a6be83b94279a4b72b904ba80a9"
+const val API_KEY =
+    "authorization: Apikey 79a0afc8e1acf0c14d61b201c82c5d491e7a8a6be83b94279a4b72b904ba80a9"
 const val APP_NAME = "CashCoin"
 
 class ApiManager {
@@ -19,6 +24,26 @@ class ApiManager {
             .build()
 
         apiService = retrofit.create(ApiService::class.java)
+    }
+
+    fun getNews(apiCallBack: ApiCallBack<ArrayList<Pair<String, String>>>) {
+
+        apiService.getTopNews().enqueue(object : Callback<NewsData> {
+            override fun onResponse(call: Call<NewsData>, response: Response<NewsData>) {
+                val data = response.body()!!
+
+                val dataToSend: ArrayList<Pair<String, String>> = arrayListOf()
+                data.data.forEach {
+                    dataToSend.add(Pair(it.title, it.url))
+                }
+                apiCallBack.onSuccess(dataToSend)
+            }
+
+            override fun onFailure(call: Call<NewsData>, t: Throwable) {
+                apiCallBack.onError(t.message!!)
+            }
+
+        })
     }
 
     interface ApiCallBack<T> {
